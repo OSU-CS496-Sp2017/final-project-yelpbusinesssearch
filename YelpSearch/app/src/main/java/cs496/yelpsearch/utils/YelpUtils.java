7dd6a89b08d229c116b693bb9bb5a8e1635c2381
Michael Rodriguez <Rodrigmi@oregonstate.edu>
2017-06-10 15:55:34 -0700

    Partially Working Parsing in YelpUtils

--- YelpSearch/app/src/main/java/cs496/yelpsearch/utils/YelpUtils.java
+++ YelpSearch/app/src/main/java/cs496/yelpsearch/utils/YelpUtils.java
@@ -1,6 +1,7 @@
 package cs496.yelpsearch.utils;
 
 import android.net.Uri;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,6 +12,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.TimeZone;
 import java.io.Serializable;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by benny on 5/31/2017.
@@ -35,9 +38,6 @@ public class YelpUtils {
         public String review_count;
         public String image_url;
         public String address1;
-        public String city;
-        public String state;
-        public String zip_code;
         public boolean is_closed;
 
     }
@@ -63,17 +63,49 @@ public class YelpUtils {
     public static ArrayList<SearchResult> parseYelpSearchResultsJSON(String searchResultsJSON) {
         try {
             JSONObject searchResultsObj = new JSONObject(searchResultsJSON);
-            JSONArray searchResultsItems = searchResultsObj.getJSONArray("items");
+            JSONArray searchResultsItems = searchResultsObj.getJSONArray("businesses");
+            Log.d(TAG, "parseYelpSearchResultsJSON searchResultsItems: " + searchResultsItems);
+            Log.d(TAG, "parseYelpSearchResultsJSON searchResultsItems Length: " + searchResultsItems.length());
 
             ArrayList<SearchResult> searchResultsList = new ArrayList<SearchResult>();
+            Log.d(TAG, "parseYelpSearchResultsJSON searchResultList: " + searchResultsList);
+
             for (int i = 0; i < searchResultsItems.length(); i++) {
                 SearchResult searchResult = new SearchResult();
                 JSONObject searchResultItem = searchResultsItems.getJSONObject(i);
-                searchResult.location = searchResultItem.getString("location");
-                searchResult.rating = searchResultItem.getString("rating");
-                searchResult.phone = searchResultItem.getString("phone");
+
+                Log.d(TAG, "parseYelpSearchResultsJSON searchResultItem: " + searchResultItem);
+                Log.d(TAG, "parseYelpSearchResultsJSON searchResultItem Length: " + searchResultItem.length());
+
+                searchResult.review_count = searchResultItem.getString("review_count");
+                Log.d(TAG, "parseYelpSearchResultsJSON review count: " + searchResult.review_count);
+
+                searchResult.phone = searchResultItem.getString("display_phone");
+                Log.d(TAG, "parseYelpSearchResultsJSON phone: " + searchResult.phone);
+
                 searchResult.price = searchResultItem.getString("price");
+                Log.d(TAG, "parseYelpSearchResultsJSON price: " + searchResult.price);
+
+
+                searchResult.image_url = searchResultItem.getString("image_url");
+                Log.d(TAG, "parseYelpSearchResultsJSON image URL: " + searchResult.image_url);
+
+                searchResult.is_closed = searchResultItem.getBoolean("is_closed");
+                Log.d(TAG, "parseYelpSearchResultsJSON is_closed: " + searchResult.is_closed);
+
+                JSONObject locationobj = searchResultItem.getJSONObject("location");
+                Log.d(TAG, "parseYelpSearchResultsJSON locationObj: " + locationobj);
+
+
+                searchResult.address1 = locationobj.getString("display_address");
+                Log.d(TAG, "parseYelpSearchResultsJSON dispay_address1: " + searchResult.address1);
+
+
+                searchResult.rating = searchResultItem.getString("rating");
+                Log.d(TAG, "parseYelpSearchResultsJSON rating: " + searchResult.rating);
+
                 searchResultsList.add(searchResult);
+                Log.d(TAG, "parseYelpSearchResultsJSON searchResultList: " + searchResultsList);
             }
             return searchResultsList;
         } catch (JSONException e) {
